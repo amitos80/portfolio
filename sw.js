41913d7010525d47f653637f66dbd3ebde4859d3
amitos80 <amitos80@gmail.com>
2020-04-24 22:38:03 +0300

    Updates

--- sw.js
+++ sw.js
@@ -42,11 +42,11 @@ self.__precacheManifest = [
   },
   {
     "url": "404.html",
-    "revision": "6a5b7ea07111ada1ab5d3f8f2d398582"
+    "revision": "b1c2ea0fbdec84f1292c96f1b60c9dc9"
   },
   {
     "url": "404/index.html",
-    "revision": "f2521fe526c9416e4261a59ee52c3ef9"
+    "revision": "3df032f82fee7be88af18d03dc20f10b"
   },
   {
     "url": "95b64a6e-7df4b477ec432349209d.js"
@@ -54,6 +54,13 @@ self.__precacheManifest = [
   {
     "url": "95b64a6e-7df4b477ec432349209d.js.map",
     "revision": "d6c63b2ca14736c51bb6d230c2300200"
+  },
+  {
+    "url": "app-164abaa8eac8740e052e.js"
+  },
+  {
+    "url": "app-164abaa8eac8740e052e.js.map",
+    "revision": "baa4f882e636227812aa4bc3337380ef"
   },
   {
     "url": "app-ec5c54057c83de9b60eb.js"
@@ -64,7 +71,7 @@ self.__precacheManifest = [
   },
   {
     "url": "chunk-map.json",
-    "revision": "3ddee5c3d28d5109995f5f4cde9bd8fa"
+    "revision": "6a643a19d8ef223bce6afcb117c15c79"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-18b9bd78e89bbe4752a5.js"
@@ -79,6 +86,13 @@ self.__precacheManifest = [
   {
     "url": "component---src-pages-404-js-f041753f241893c6a803.js.map",
     "revision": "f3c2f59e70f621e379afc2ba8eb4fea1"
+  },
+  {
+    "url": "component---src-pages-index-js-34dd0ff7393825a967c4.js"
+  },
+  {
+    "url": "component---src-pages-index-js-34dd0ff7393825a967c4.js.map",
+    "revision": "290330ff6e6107b6b6b6ff29f68d4ef1"
   },
   {
     "url": "component---src-pages-index-js-fcf8027589fbaa734401.js"
@@ -135,15 +149,15 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "9215051407b601cb80a741f8a9822503"
+    "revision": "0b37db96f64d8f1eae9cd7d572c40c10"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "79030751105fba0881c2df2d7b5a66ca"
+    "revision": "710ca6d012054da53ae5d27f11226fb6"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "09ee42860c3d0ef5e3174e9f3ae5c37a"
+    "revision": "e3d6284572ee136f6311b17cc4f7b183"
   },
   {
     "url": "page-data/404.html/page-data.json",
@@ -155,7 +169,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c628f5f075112896faf48f76ba2d912e"
+    "revision": "d8572b508a1b8ba1673846e7e2f7661a"
   },
   {
     "url": "page-data/index/page-data.json",
@@ -203,6 +217,13 @@ self.__precacheManifest = [
     "url": "styles.28c8650698d995b47809.css"
   },
   {
+    "url": "webpack-runtime-829b9e0ebe135d21f841.js"
+  },
+  {
+    "url": "webpack-runtime-829b9e0ebe135d21f841.js.map",
+    "revision": "52e68cec91c58fda974299fb343cb333"
+  },
+  {
     "url": "webpack-runtime-8ef48d81a6e026c61eb9.js"
   },
   {
@@ -211,7 +232,7 @@ self.__precacheManifest = [
   },
   {
     "url": "webpack.stats.json",
-    "revision": "b2d56502a726ef15e808b4bd8ea8f80a"
+    "revision": "37b558d56323da1803143fe0672b18cf"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -299,12 +320,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^`), ``)
+  pathname = pathname.replace(new RegExp(`^/protfolio`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-ec5c54057c83de9b60eb.js`))) {
+  if (!resources || !(await caches.match(`/protfolio/app-164abaa8eac8740e052e.js`))) {
     return await fetch(event.request)
   }
 
@@ -317,7 +338,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/protfolio/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
