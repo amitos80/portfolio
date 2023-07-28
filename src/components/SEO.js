838c9e78f110d86242d386ed21f710015f960d78
amitos80 <amitos80@gmail.com>
2023-07-28 04:56:36 +0300

    SEO added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -6,10 +6,10 @@ const SEO = () => {
 
     const site = {
         siteMetadata: {
-            title: '',
-            description: '',
-            author: '',
-            image: ''
+            title: 'Amit Friedberg - Full Stack Web Developer',
+            description: 'Welcome to my portfolio website! As a dynamic and innovative full-stack web developer, I am dedicated to providing top-notch web product development services to ambitious startup companies in the software industry. With a passion for crafting cutting-edge digital experiences, I strive to transform ideas into reality through seamless code and intuitive user interfaces. Explore my diverse projects and discover how my technical expertise and creative flair can elevate your business to new heights. Let\'s collaborate and build the future together!',
+            author: 'Amit Friedberg',
+            image: 'https://amitos80.github.io/static/aec40c5d062faba36e6e805e0c1a7dd6/b3a76/photo.png'
         }
     }
   const title = site.siteMetadata.title;
